e8160812db4880ca9a296790025940282bf76543
krishnendu1154 <60266755+krishnendu1154@users.noreply.github.com>
2020-06-26 21:05:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var a = prompt("Enter UserName");
+var b = prompt("Enter password");
+if (a == "pramalika" && b == "bacchha") {
+    window.location.href = "start.html";
+} else {
+    alert("invalid details");
+    window.location.href = "index.html";
+}
